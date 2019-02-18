ced97a323af738770df8e594437e3efebfd6d70a
徐安小调 <javaandswing@163.com>
2019-02-18 10:48:45 +0800

    Update mxGraphTransferable.java
    fixed java.lang.ClassNotFoundException: com/mxgraph/swing/util/mxGraphTransferable

--- java/src/com/mxgraph/swing/util/mxGraphTransferable.java
+++ java/src/com/mxgraph/swing/util/mxGraphTransferable.java
@@ -528,8 +528,14 @@ public class mxGraphTransferable implements Transferable, UIResource,
 
 		try
 		{
-			dataFlavor = new DataFlavor(DataFlavor.javaSerializedObjectMimeType
-					+ "; class=com.mxgraph.swing.util.mxGraphTransferable");
+			{
+//			dataFlavor = new DataFlavor(DataFlavor.javaSerializedObjectMimeType
+//					+ "; class=com.mxgraph.swing.util.mxGraphTransferable");
+
+			//fixed java.lang.ClassNotFoundException: com/mxgraph/swing/util/mxGraphTransferable
+			dataFlavor = new DataFlavor(DataFlavor.javaJVMLocalObjectMimeType
+					+ "; class=com.mxgraph.swing.util.mxGraphTransferable", null,
+					new com.mxgraph.swing.util.mxGraphTransferable(null, null).getClass().getClassLoader());
 		}
 		catch (ClassNotFoundException e)
 		{
